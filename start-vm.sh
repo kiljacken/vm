adc84b7c5bfb8433c72a67e9645326aa2afa6906
Emil Lauridsen <mine809@gmail.com>
2018-06-22 19:33:20 +0200

    stash

--- start-vm.sh
+++ start-vm.sh
@@ -14,14 +14,11 @@ NET_DEV_MAC="35:5A:5F:2C:B9:4A"
 
 #OVMF_CODE_PATH="/usr/share/ovmf/ovmf_code_x64.bin"
 #OVMF_VARS_PATH="/usr/share/ovmf/ovmf_vars_x64.bin"
-OVMF_CODE_PATH="/home/kiljacken/vm/ovmf/OVMF_CODE-pure-efi.fd"
-OVMF_VARS_PATH="/home/kiljacken/vm/ovmf/OVMF_VARS-pure-efi.fd"
+OVMF_CODE_PATH="/home/kiljacken/vm/ovmf/OVMF_CODE.fd"
+OVMF_VARS_PATH="/home/kiljacken/vm/ovmf/OVMF_VARS.fd"
 
 OVMF_TEMP_VARS=/tmp/ovmf_vars_${VM_NAME}.fd
 cp $OVMF_VARS_PATH $OVMF_TEMP_VARS
-
-DISK_NAME="vfio-win10.raw"
-DISK_FORMAT="raw"
 
 EVDEV_KEYBOARD="/dev/input/event4"
 EVDEV_MOUSE="/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse"
@@ -30,9 +27,7 @@ export QEMU_AUDIO_DRV=pa
 export QEMU_PA_SERVER=/run/user/1000/pulse/native
 
 TOTAL_CORES='0-11'
-TOTAL_CORES_MASK=FFF            # 0-11, bitmask 0b111111111111
 HOST_CORES='0-1,6-7'            # Cores reserved for host
-HOST_CORES_MASK=C3              # 0-1,6-7, bitmask 0b000011000011
 VIRT_CORES='2-5,8-11'           # Cores reserved for virtual machine(s)
 
 HUGEPAGES_SIZE=$(grep Hugepagesize /proc/meminfo | awk {'print $2'})
@@ -40,25 +35,17 @@ HUGEPAGES_SIZE=$((HUGEPAGES_SIZE * 1024))
 
 VM_HUGEPAGES_NEED=$(( VM_MEMORY / HUGEPAGES_SIZE ))
 
-shield_vm() {
-    cset set -c $TOTAL_CORES -s machine.slice
-    # Shield two cores cores for host and rest for VM(s)
-    cset shield --kthread on --cpu $VIRT_CORES
-}
-
-unshield_vm() {
-    echo $TOTAL_CORES_MASK > /sys/bus/workqueue/devices/writeback/cpumask
-    cset shield --reset
-}
-
 setup_networking() {
     ip tuntap add dev $NET_TAP_NAME mode tap
-    ip link set dev $NET_TAP_NAME address '12:c7:b3:1c:eb:34'
     ip addr add 172.20.0.1/24 dev $NET_TAP_NAME
+    ip route add 172.20.0.2 dev $NET_TAP_NAME
+
+    ip link set dev $NET_TAP_NAME address '12:c7:b3:1c:eb:34'
     ip link set $NET_TAP_NAME up
 
+    sysctl net.ipv4.conf."$NET_TAP_NAME".proxy_arp=1
+    sysctl net.ipv4.conf."$NET_INTERFACE".proxy_arp=1
     sysctl net.ipv4.ip_forward=1
-    sysctl net.ipv4.conf."$NET_INTERFACE".forwarding=1
 
     iptables -t nat -A POSTROUTING -o $NET_INTERFACE -j MASQUERADE
     iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
@@ -70,7 +57,13 @@ teardown_networking() {
     iptables -D FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     iptables -D FORWARD -i $NET_TAP_NAME -o $NET_INTERFACE -j ACCEPT
 
+    sysctl net.ipv4.conf."$NET_TAP_NAME".proxy_arp=0
+    sysctl net.ipv4.conf."$NET_INTERFACE".proxy_arp=0
+    sysctl net.ipv4.ip_forward=0
+
     ip link set $NET_TAP_NAME down
+    
+    ip route del 172.20.0.2 dev $NET_TAP_NAME
     ip tuntap del $NET_TAP_NAME mode tap
 }
 
@@ -108,50 +101,45 @@ set_governor() {
 
 PCI_BUS_VGA="01:00.0"
 PCI_BUS_SND="01:00.1"
+PCI_BUS_NVME="06:00.0"
 
 #attach_to_vfio "$PCI_BUS_VGA" "1002 67df"
 #attach_to_vfio "$PCI_BUS_SND" "1002 aaf0"
+attach_to_vfio "$PCI_BUS_NVME" "144d a804"
 setup_networking
 
 echo 3 > /proc/sys/vm/drop_caches
 echo 1 > /proc/sys/vm/compact_memory
 
 sysctl vm.nr_hugepages=$VM_HUGEPAGES_NEED
-
-shield_vm
-
 sysctl vm.stat_interval=120
-
 sysctl -w kernel.watchdog=0
-# the kernel's dirty page writeback mechanism uses kthread workers. They introduce
-# massive arbitrary latencies when doing disk writes on the host and aren't
-# migrated by cset. Restrict the workqueue to use only cpu 0.
-echo $HOST_CORES_MASK > /sys/bus/workqueue/devices/writeback/cpumask
 # THP can allegedly result in jitter. Better keep it off.
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 # Force P-states to P0
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo 0 > /sys/bus/workqueue/devices/writeback/numa
->&2 echo "VM Shielded"
 
 #allocate_hugepages
 #set_governor "performance"
 
-
 # Pin CPU cores
-#cset set -c 0,1,6,7 -s system
-#cset set -c 0-11 -s vm
-#cset proc --force -m -k -f root -t system
+cset set -c $HOST_CORES -s system
+cset set -c $VIRT_CORES -s vm
+cset proc --force -m -k -f root -t system
+
+# Switch monitor to DP input
+modprobe i2c_dev
+ddcutil setvcp 60 0x0f
 
 echo "Starting QEMU"
-#cset proc -e -s vm --
-taskset -c $TOTAL_CORES qemu-system-x86_64 \
+cset proc -e -s vm -- qemu-system-x86_64 \
     -name $VM_NAME \
     -nodefaults \
     -nodefconfig \
     -no-user-config \
     -enable-kvm \
-    -cpu host,hv_relaxed,hv_vapic,hv_spinlocks=0x1fff,hv_time \
+    -cpu host,kvm=off,hv_relaxed,hv_vapic,hv_spinlocks=0x1fff,hv_time,hv_vendor_id=nuckfvidia \
     -smp cpus=8,sockets=1,cores=4,threads=2 \
     -machine pc-i440fx-2.11,accel=kvm,mem-merge=off \
     -vcpu vcpunum=0,affinity=2 -vcpu vcpunum=1,affinity=8 \
@@ -165,6 +153,7 @@ taskset -c $TOTAL_CORES qemu-system-x86_64 \
     -drive file=$OVMF_TEMP_VARS,if=pflash,format=raw \
     -vga none \
     -nographic \
+    -monitor stdio \
     -rtc base=localtime,clock=host,driftfix=slew \
     -no-hpet \
     -object input-linux,id=mouse1,evdev=$EVDEV_MOUSE \
@@ -172,19 +161,23 @@ taskset -c $TOTAL_CORES qemu-system-x86_64 \
     -device ioh3420,bus=pci.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root \
     -device vfio-pci,host=$PCI_BUS_VGA,bus=root,addr=00.0,multifunction=on \
     -device vfio-pci,host=$PCI_BUS_SND,bus=root,addr=00.1 \
+    -device vfio-pci,host=$PCI_BUS_NVME,bus=root,addr=01.0 \
     -device virtio-net-pci,netdev=net0,mac=$NET_DEV_MAC \
     -netdev tap,id=net0,ifname=$NET_TAP_NAME,script=no,downscript=no \
     -soundhw hda \
     -object iothread,id=iothread0 \
     -device virtio-scsi-pci,id=scsi,iothread=iothread0 \
-    -drive file=$DISK_NAME,id=disk0,format=$DISK_FORMAT,if=none,cache=none,aio=native -device scsi-hd,bus=scsi.0,drive=disk0 \
-    -drive file=/dev/sda,id=disk1,format=raw,if=none,cache=writeback,aio=threads -device scsi-hd,bus=scsi.0,drive=disk1
-    #-drive file=./Win10_1709_EnglishInternational_x64.iso,media=cdrom \
-    #-drive file=./virtio-win-0.1.141.iso,media=cdrom
+    -drive file=/dev/disk/by-id/ata-ST31000524AS_9VPDNR3W,id=disk0,format=raw,if=none,cache=none,aio=native -device scsi-hd,bus=scsi.0,drive=disk0 \
+    -drive file=./Win10_1803_EnglishInternational_x64.iso,media=cdrom \
+    -drive file=./virtio-win-0.1.141.iso,media=cdrom
+
+# Switch monitor to HDMI input
+modprobe i2c_dev
+ddcutil setvcp 60 0x11
 
 # Unpin CPU cores
-#cset set -d system
-#cset set -d vm
+cset set -d system
+cset set -d vm
 
 #set_governor "powersave"
 #free_hugepages
@@ -192,11 +185,10 @@ taskset -c $TOTAL_CORES qemu-system-x86_64 \
 sysctl vm.nr_hugepages=0
 sysctl vm.stat_interval=1
 sysctl -w kernel.watchdog=1
-unshield_vm
 echo always > /sys/kernel/mm/transparent_hugepage/enabled
 echo powersave | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo 1 > /sys/bus/workqueue/devices/writeback/numa
->&2 echo "VMs UnShielded"
 
 teardown_networking
 #detach_from_vfio "$PCI_BUS_VGA" "$PCI_BUS_SND"
+detach_from_vfio "$PCI_BUS_NVME"
